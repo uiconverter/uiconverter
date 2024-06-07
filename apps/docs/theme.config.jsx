386f9643317d6007856afe81d266edfe7f8d7707
chaaerim <cofla411@ewhain.net>
2024-06-07 15:03:56 +0900

    feat: set nextra at docs workspace

---
+++ apps/docs/theme.config.jsx
@@ -0,0 +1,8 @@
+/** @type {import('nextra-theme-docs').DocsThemeConfig} */
+export default {
+    logo: <span>UIconverter</span>,
+    project: {
+      link: 'https://github.com/UIconverter/uiconverter'
+    }
+    // ... other theme options
+  }

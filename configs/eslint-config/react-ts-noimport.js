c0986b2d71a813cba61bf2312cd05c7798acd351
chaaerim <cofla411@ewhain.net>
2024-07-03 13:28:27 +0900

    feat: config workspace 아래 eslint-config 추가

---
+++ configs/eslint-config/react-ts-noimport.js
@@ -0,0 +1,4 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: ['./react.js', '@sickgyun/eslint-config-ts/noimport'],
+};
---
+++ configs/eslint-config/react-ts.js
@@ -0,0 +1,4 @@
+/** @type {import('eslint').Linter.Config} */
+module.exports = {
+  extends: ['./react.js', '@uiconverter/eslint-config-ts'],
+};
